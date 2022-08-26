cb5b8d645646502608941bb03dc3fc1f7bd18f7c
Eddmafra <eduardo_mafra@hotmail.com>
2022-08-26 18:37:03 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const listaDeManagers = [];
+  employees.forEach((elemento) => elemento.managers
+    .forEach((e) => listaDeManagers.push(e)));
+  return listaDeManagers.some((i) => i === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((elemento) => elemento.managers.includes(managerId))
+    .map((pessoas) => `${pessoas.firstName} ${pessoas.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
