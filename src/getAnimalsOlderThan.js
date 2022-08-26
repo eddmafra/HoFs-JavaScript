767323d9d861ec731e6b9af04b5b361e8371fd93
Eddmafra <eduardo_mafra@hotmail.com>
2022-08-26 16:26:28 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalProcurado = species.find((elemento) => elemento.name === animal);
+  // console.log(animalProcurado.residents);
+  return animalProcurado.residents.every((objeto) => objeto.age > age);
+  // (elemento.age > age) ? );
 }
-
+// console.log(getAnimalsOlderThan('lions', 2));
 module.exports = getAnimalsOlderThan;
