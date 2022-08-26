d8535efa2dee3318c2cc1625809e732c612b46b9
Eddmafra <eduardo_mafra@hotmail.com>
2022-08-26 15:25:49 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.reduce((acc, curr) => {
+    const especies = species.find((animal) => animal.id === curr);
+    acc.push(especies);
+    return acc;
+  }, []);
 }
 
 module.exports = getSpeciesByIds;
