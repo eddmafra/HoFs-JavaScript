4f2659ef56c1613c55b293c19349fbb3c74f77af
Eddmafra <eduardo_mafra@hotmail.com>
2022-08-27 12:08:04 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, cur) => {
+      acc[cur.name] = cur.residents.length;
+      return acc;
+    }, {});
+  }
+  if (Object.keys(animal).length === 1) {
+    return species.filter((elemento) => elemento.name === Object.values(animal)[0])
+      .reduce((acc, cur) => acc + cur.residents.length, 0);
+  }
+  if (Object.keys(animal).length === 2) {
+    return species.filter((elemento) => elemento.name === Object.values(animal)[0])[0].residents
+      .filter((e) => e.sex === Object.values(animal)[1]).length;
+  }
 }
 
+// console.log(countAnimals({ specie: 'lions', sex: 'male' }));
 module.exports = countAnimals;
