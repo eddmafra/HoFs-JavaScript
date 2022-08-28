58a4cc249dd55ff2730f0124d8676944194c2070
Eddmafra <eduardo_mafra@hotmail.com>
2022-08-27 22:18:17 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const crianca = entrants.filter((element) => element.age < 18);
+  const adulto = entrants.filter((elemento) => elemento.age >= 18 && elemento.age < 50);
+  const veio = entrants.filter((e) => e.age >= 50);
+  const entrada = {
+    child: crianca.length,
+    adult: adulto.length,
+    senior: veio.length,
+  };
+  return entrada;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  return Object.values(countEntrants(entrants))[0] * 20.99
+  + Object.values(countEntrants(entrants))[1] * 49.99
+  + Object.values(countEntrants(entrants))[2] * 24.99;
 }
 
 module.exports = { calculateEntry, countEntrants };
+
+calculateEntry([
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+]);
